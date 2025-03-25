fb5580811cfe7e334dbc85d9336211dadcd391fd
camlopes <camalopes.cat@gmail.com>
2025-03-25 17:05:15 -0300

    Metodo responsavel por retornar os simbolos

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
 
-        int total = 100;
+        int saldo = 100;
         int aposta;
         int lucro;
         String[] simbolos = {};
@@ -15,8 +16,37 @@ public class Main {
         System.out.println("Simbolos: 🍒 🍉 🍋 🔔 ⭐ ");
         System.out.println("*************************");
 
+        while(saldo > 0){
+            System.out.println("Valor atual do saldo: R$" + saldo);
+            System.out.print("Digite um valor de aposta: ");
+            aposta = scanner.nextInt();
 
+            if(aposta > saldo){
+                System.out.println("Saldo insuficiente");
+                continue;
+            } else if(aposta <= 0){
+                System.out.println("Aposta deve ser maior que 0");
+                continue;
+            } else {
+                saldo -= aposta;
+            }
+
+            System.out.println("Rodando...");
+            rodando();
+        }
 
         scanner.close();
+    }
+
+    static String[] rodando(){
+        String[] simbolos = {"🍒", "🍉", "🍋", "🔔", "⭐"};
+        String[] rodada = new String[3];
+        Random random = new Random();
+
+        for(int i = 0; i < 3; i++){
+            rodada[i] = simbolos[random.nextInt(simbolos.length)];
+        }
+
+        return rodada;
     }
 }

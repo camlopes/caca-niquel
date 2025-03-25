0b989a61f28dd5817fef5d1283dc8d836f4a06e6
camlopes <camalopes.cat@gmail.com>
2025-03-25 16:36:44 -0300

    Abertura do programa

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        int total = 100;
+        int aposta;
+        int lucro;
+        String[] simbolos = {};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("*************************");
+        System.out.println("Bem vindo ao Caça-níquel ");
+        System.out.println("Simbolos: 🍒 🍉 🍋 🔔 ⭐ ");
+        System.out.println("*************************");
+
+
+
+        scanner.close();
+    }
+}
